bdfafcb648c21e8c9849695bad842f67540a8200
12141391 <>
2016-11-22 19:12:03 +0700

    Membuat array untuk nama pemain dan sekor pemain. Selanjutnya membuat inputan dengan cara instanisasi class Scanner. Berikutnya instanisasi method random untuk mengacak nilai inputan. Terakhir deklarasi variabel tipe string untuk inputan y (keluar/exit). Tidak hanya itu, library utiliti ArrayList, Random dan Scanner juga kami inport.

---
+++ com.blogspot.karisnaema.app12141391/src/com/blogspot/karisnaema/app12141391/ComBlogspotKarisnaemaApp12141391.java
@@ -0,0 +1,18 @@
+package com.blogspot.karisnaema.app12141391;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class ComBlogspotKarisnaemaApp12141391 {
+
+    public static void main(String[] args) {
+        ArrayList <String> namaPemain = new ArrayList<>(); // Membuat array Nama Pemain
+        ArrayList <Integer> sekorPermainan = new ArrayList<>(); // Membuat array Sekor Pemain
+        Scanner input = new Scanner(System.in); // Input pertama
+        Scanner input2= new Scanner(System.in); // Input kedua
+        Random angkaRandom=new Random(); // Mengacak angka
+        String cariY; // Untuk mencari nilai Y, jika input y, maka otomatis akan keluar
+    }
+    
+}
